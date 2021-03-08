0d8d4d7eb1a407cfaa54b3232ef8ca9051bfd1e9
Bruno Navarro <brunonavarro34@gmail.com>
2021-03-08 08:57:37 -0500

    Conectando SqlDelight a repositorios con kodein en Arquitectura MVVM

--- app/src/main/java/com/brunonavarro/smiledu/data/repository/taskRepository/TaskInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.brunonavarro.smiledu.data.repository.taskRepository
-
-import com.brunonavarro.smiledu.data.entity.Task
-
-interface TaskInterface {
-    suspend fun getTasks(): List<Task>
-    suspend fun updateTask(task: Task)
-    suspend fun addTask(task: Task)
-}
